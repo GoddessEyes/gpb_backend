2f1a8515d5cee8e0e7275e4216c0969b158845e4
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-06-20 04:42:21 +1000

    CORS error fix

--- src/breathtaking/settings/environment/dev.py
+++ src/breathtaking/settings/environment/dev.py
@@ -11,6 +11,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CORS_ALLOW_CREDENTIALS = False
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_HEADERS = list(default_headers)
 
 CORS_ALLOW_METHODS = list(default_methods)
