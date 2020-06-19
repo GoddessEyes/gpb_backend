e96896e0ef75cfdae948304062f09ade665f016e
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-06-20 01:34:50 +1000

    Static conf for heroku

--- src/breathtaking/settings/environment/base.py
+++ src/breathtaking/settings/environment/base.py
@@ -3,6 +3,13 @@ import os
 from breathtaking.settings.components.common import *
 from breathtaking.settings.components.db import *
 
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)),
+)
+SRC_DIR = os.path.join(BASE_DIR, '..')
+
+_ENV_VAR = 'VAR_PATH'
+VAR_PATH = os.getenv(_ENV_VAR, os.path.join(SRC_DIR, 'var'))
 
 DEBUG = True
 
@@ -62,3 +69,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(VAR_PATH, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(VAR_PATH, 'media')
+
+for path in (VAR_PATH, MEDIA_ROOT):
+    if not os.path.exists(path):
+        os.mkdir(path)
