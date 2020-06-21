4777cd1a881a798786a3d0e24dfa2baaf1e47279
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-06-21 14:48:15 +1000

    Added serialization tags and theme for userprofile

--- src/breathtaking/api/v1/userprofile/serializers.py
+++ src/breathtaking/api/v1/userprofile/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from breathtaking.api.v1.auth.serializers import UserSerializer
+from breathtaking.api.v1.ideas.serializers import ThemeSerializer, TagSerializer
 from breathtaking.api.v1.stats.serializers import StatsSerializer
 from breathtaking.modules.eauth.models import User
 from breathtaking.modules.ideas.models import IdeaOffer
@@ -8,11 +9,21 @@ from breathtaking.modules.solutions.models import Solution
 
 
 class UserProfileIdeaOfferSerializer(serializers.ModelSerializer):
+    themes_info = serializers.SerializerMethodField(read_only=True)
+    tags_info = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = IdeaOffer
         exclude = (
             'user',
         )
+
+
+    def get_themes_info(self, instance):
+        return ThemeSerializer(instance.themes).data
+
+    def get_tags_info(self, instance):
+        return TagSerializer(instance.tags, many=True).data
 
 
 class UserProfileSolutionSerializer(serializers.ModelSerializer):
